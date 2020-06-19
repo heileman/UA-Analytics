c96ebbc2c0e0030b98423ddf29d453fed81400c3
Jiacheng Zhang <jiachengzhang1@email.arizona.edu>
2020-06-18 22:00:45 -0700

    delete console log

--- Program-Opporunity/v1/js/handlers.js
+++ Program-Opporunity/v1/js/handlers.js
@@ -28,7 +28,7 @@ function handlePoppedBubbleOnClick(d) {
 
 function handleFilterButtonOnClick(e) {
   const right = document.getElementById("existing-program-filters").style.right;
-  console.log(right);
+
   if (right === "-370px" || right === "") {
     document.getElementById("existing-program-filters").style.right = "0px";
     e.innerHTML = "Hide University Program Filters";
